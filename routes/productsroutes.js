1da0e450d7361c6f4abcb1166833665627ed258e
Chaz-C <chazcastro@Chazs-MacBook-Pro.local>
2017-01-13 20:51:14 -1000

    GET id working

--- routes/productsroutes.js
+++ routes/productsroutes.js
@@ -9,22 +9,39 @@ let productsArr = productsDb.allProducts;
 
 let newId = 0;
 
-function allIds() {
+function findIndex(id) {
   let idArr = [];
+
   for ( let i = 0; i < productsArr.length; i ++ ) {
     idArr.push(productsArr[i].id);
   }
-  return idArr;
+
+  if ( idArr.indexOf(parseInt(id)) > -1 ) {
+    return idArr.indexOf(parseInt(id));
+  } else {
+    return false;
+  }
 }
+
+router.get('/', (req, res) => {
+  res.render('index', productsDb);
+});
+
+router.get('/:id', (req, res) => {
+  let index = findIndex(req.params.id);
+  console.log('index', index);
+  res.render('product', productsArr[index]);
+});
 
 router.post('/', (req, res) => {
   let newItem = req.body;
   newItem.id = newId;
   productsArr.push(newItem);
-  res.render('index', productsDb);
-  console.log(productsArr);
+  res.redirect('/products');
   newId++;
 });
+
+
 
 
 module.exports = router;
